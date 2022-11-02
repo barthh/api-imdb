1cecb8bef0c0dda6da26ae42d3f78d920bed3858
Barthh <barthelemy.popille@epfedu.fr>
2022-11-02 07:37:26 +0100

    Delete useless file

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-versioning_events = package.VersioningEventFacade.get_movies("inception")
-# # for versioning_event in versioning_events:
-# #     assert isinstance(versioning_event, package.VersioningEvent)
-# # print(versioning_events)
-
-# for versioning_event in versioning_events:
-#     print(versioning_event.id, versioning_event.title, versioning_event.description)
