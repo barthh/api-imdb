eaef646f9f0c609152059df02995d1198d6216ea
DimitriPrieu <dimitri.prieur155@hotmail.fr>
2022-11-12 11:21:19 +0100

    Updating test with errorMessage for APIKey

--- tests/package_test.py
+++ tests/package_test.py
@@ -1,5 +1,6 @@
 # main.py
 from package import versioning_event_facade
+from package import imdb_requests_event
 
 # Define test here
 def test_score():
@@ -9,3 +10,9 @@ def test_score():
     assert title_rating.fullTitle == "leon the professional"
     assert title_rating.rating == "8.5"
     assert title_rating.errorMessage == ""
+
+
+def test_APIKey():
+    APIkey_message = imdb_requests_event.IMDBRequest.search_movies("leon").errorMessage
+
+    assert APIkey_message != "Invalid API Key"
