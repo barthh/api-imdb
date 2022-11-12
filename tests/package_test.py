5821b79a01b4692cdae58956664a4c1ebf72d0f5
DimitriPrieu <dimitri.prieur155@hotmail.fr>
2022-11-12 10:25:40 +0100

    Correcting test

--- tests/package_test.py
+++ tests/package_test.py
@@ -1,7 +1,11 @@
 # main.py
-import package
+from package import versioning_event_facade
 
 # Define test here
+def test_score():
+    title_rating = versioning_event_facade.RatingsEvent("leon the","leon the professional","8.5","")
 
-def test():
-    assert package.VersioningEventFacade.get_rating("leon the professional").imdb_score == '8.5'
+    assert title_rating.title == "leon the"
+    assert title_rating.fullTitle == "leon the professional"
+    assert title_rating.rating == "8.5"
+    assert title_rating.errorMessage == ""
