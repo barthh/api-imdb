426a45f39943c8cf3d74cff78f99032406576037
Barthh <barthelemy.popille@epfedu.fr>
2022-11-12 16:50:40 +0100

    Error handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from .views import *
-from flask import render_template, request
-
 
 app = Flask(__name__)
 app.register_blueprint(home_app)
 app.register_blueprint(information_app)
 app.register_blueprint(search_app)
 
-from app import error_handlers
+from app.views import error_handlers
--- app/views/error_handlers.py
+++ app/views/error_handlers.py
@@ -1,10 +1,10 @@
 from app import app
 from flask import render_template, request
+from flask import abort
 
 @app.errorhandler(404)
-def page_not_found(e):
-    print(e)
-    return render_template("error404.html")
+def page_not_found(error):
+    return render_template('error404.html'), 404
 
 @app.errorhandler(500)
 def page_not_found(e):
