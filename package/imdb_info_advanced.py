eb392a3e4f0a788fadc561beed1d27bc18b81f2d
Barthh <barthelemy.popille@epfedu.fr>
2022-11-13 03:35:59 +0100

    Fix problems

--- package/imdb_info_advanced.py
+++ package/imdb_info_advanced.py
@@ -2,19 +2,20 @@ from datetime import datetime
 
 class AdvancedEvent:
 
-    def __init__(self, title, year, image, releaseDate, time, plot, genres, ratings, boxOffice, directors, writers, stars, keywords, similars, errorMessage):
+    def __init__(self, title, year, image, releaseDate, time, plot, genres, ratings, directors, writers, stars, keywords, similars, errorMessage):
         self.title = title
         self.year = year
         self.image = image
 
         self.releaseDate = releaseDate
-        date = datetime.strptime(releaseDate, "%Y-%m-%d")
-        self.releaseDateFormatted  = datetime.strftime(date, "%d %B %Y" )
-        
+        if releaseDate:
+            date = datetime.strptime(releaseDate, "%Y-%m-%d")
+            self.releaseDateFormatted  = datetime.strftime(date, "%d %B %Y" )
+        else:
+            self.releaseDateFormatted = None
         self.time= time
         self.plot = plot
         self.genres = genres
-        self.boxOffice = boxOffice
         self.directors = directors
         self.writers = writers
         self.stars = stars
@@ -22,14 +23,19 @@ class AdvancedEvent:
         self.similars = similars
         self.errorMessage = errorMessage
 
-        # Keep only ratings
-        ratings = { key: ratings[key] for key in ["imDb", "metacritic", "theMovieDb", "rottenTomatoes", "filmAffinity"] }
-        self.ratings = ratings
+        if ratings:
+            # Keep only ratings
+            ratings = { key: ratings[key] for key in ["imDb", "metacritic", "theMovieDb", "rottenTomatoes", "filmAffinity"] }
+            self.ratings = ratings
 
-        # Convert /10 to /100 notation (for purcentage)
-        ratingsPurcentage = ratings.copy()
-        elements = ["imDb", "theMovieDb", "filmAffinity"] 
-        for element in elements:
-            if ratingsPurcentage[element]:
-                ratingsPurcentage[element] = int(float(ratingsPurcentage[element]) * 10)
-        self.ratingsPurcentage = ratingsPurcentage
+            # Convert from /10 to /100 notation (to purcentage)
+            ratingsPurcentage = ratings.copy()
+            elements = ["imDb", "theMovieDb", "filmAffinity"] 
+            for element in elements:
+                if ratingsPurcentage[element]:
+                    ratingsPurcentage[element] = int(float(ratingsPurcentage[element]) * 10)
+            self.ratingsPurcentage = ratingsPurcentage
+        else:
+            self.ratings = None
+            self.ratingsPurcentage = None
+    
